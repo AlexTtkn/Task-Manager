f22f93cd2a6c6bafa92d05e3b16e5144445f2a60
Alex <alextetekin@gmail.com>
2024-01-05 05:38:53 +0300

    build: added Auth configs

---
+++ app/src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -0,0 +1,30 @@
+package hexlet.code.app.component;
+
+import hexlet.code.app.dto.UserDTO.UserCreateDto;
+import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var userData = new UserCreateDto();
+        userData.setEmail("hexlet@example.com");
+        userData.setPasswordDigest("qwerty");
+        var user = userMapper.map(userData);
+        userRepository.save(user);
+    }
+}
