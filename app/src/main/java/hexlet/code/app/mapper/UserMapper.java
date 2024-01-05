f0578fc7c4039908e6308570ab7a01409fc0ed63
Alex <alextetekin@gmail.com>
2024-01-05 05:39:34 +0300

    build: changed UserMapper

--- app/src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -4,7 +4,15 @@ import hexlet.code.app.dto.UserDTO.UserCreateDto;
 import hexlet.code.app.dto.UserDTO.UserDto;
 import hexlet.code.app.dto.UserDTO.UserUpdateDto;
 import hexlet.code.app.model.User;
-import org.mapstruct.*;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.BeforeMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
@@ -13,9 +21,19 @@ import org.mapstruct.*;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public abstract User map(UserCreateDto userCreateDto);
 
     public abstract UserDto map(User user);
 
     public abstract void update(UserUpdateDto data, @MappingTarget User model);
+
+    @BeforeMapping
+    public void encryptPassword(UserCreateDto data) {
+        var password = data.getPasswordDigest();
+        data.setPasswordDigest(passwordEncoder.encode(password));
+    }
 }
