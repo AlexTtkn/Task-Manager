fc5f53a95823c649970090671374aae104f742a7
Alex <alextetekin@gmail.com>
2024-01-15 11:57:46 +0300

    fix: fix some problems

--- src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -9,8 +9,6 @@ import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = {JsonNullableMapper.class, ReferenceMapper.class},
