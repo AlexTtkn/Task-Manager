100afdf360cf872478795b829f6d1ec6d93c379e
Alex <alextetekin@gmail.com>
2024-01-15 11:23:05 +0300

    test: change UserControllerTest

--- src/test/java/hexlet/code/app/controller/UserControllerTest.java
+++ src/test/java/hexlet/code/app/controller/UserControllerTest.java
@@ -1,36 +1,29 @@
 package hexlet.code.app.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.util.ModelGenerator;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.instancio.Instancio;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-
-import java.time.ZoneId;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.datafaker.Faker;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -65,11 +58,14 @@ class UserControllerTest {
         userRepository.save(testUser);
     }
 
+//    @AfterEach
+//    public void cleanUp() {
+//        userRepository.deleteById(testUser.getId());
+//    }
+
 
     @Test
     public void testShow() throws Exception {
-        var createdAt = java.util.Date.from(testUser.getCreatedAt().atStartOfDay()
-                .atZone(ZoneId.systemDefault()).toInstant());
 
         var request = get("/api/users/{id}", testUser.getId()).with(jwt());
         var result = mockMvc.perform(request)
