ae17523892c01860a69171d34da7840decb7f022
Alex <alextetekin@gmail.com>
2024-01-18 09:59:12 +0300

    build: add default label.

--- src/main/java/hexlet/code/app/component/DataInitializer.java
+++ src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -1,9 +1,12 @@
 package hexlet.code.app.component;
 
+import hexlet.code.app.dto.LabelDTO.LabelCreateDTO;
 import hexlet.code.app.dto.TaskStatusDTO.TaskStatusCreateDTO;
 import hexlet.code.app.dto.UserDTO.UserCreateDTO;
+import hexlet.code.app.mapper.LabelMapper;
 import hexlet.code.app.mapper.TaskStatusMapper;
 import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.repository.LabelRepository;
 import hexlet.code.app.repository.TaskStatusRepository;
 import hexlet.code.app.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -22,15 +25,18 @@ public class DataInitializer implements ApplicationRunner {
     private UserMapper userMapper;
     private TaskStatusRepository taskStatusRepository;
     private TaskStatusMapper taskStatusMapper;
+    private LabelRepository labelRepository;
+    private LabelMapper labelMapper;
     private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         addAdminUser();
         addDefaultSlugs();
+        addDefaultLabels();
     }
 
-    public void addAdminUser() {
+    private void addAdminUser() {
         var userData = new UserCreateDTO();
         userData.setEmail("hexlet@example.com");
         userData.setPasswordDigest("qwerty");
@@ -40,7 +46,7 @@ public class DataInitializer implements ApplicationRunner {
         userRepository.save(user);
     }
 
-    public void addDefaultSlugs() {
+    private void addDefaultSlugs() {
         List<String> defaultSlugs = List.of("draft", "to_review", "to_be_fixed", "to_publish", "published");
         defaultSlugs.forEach(slug -> {
             var statusData = new TaskStatusCreateDTO();
@@ -58,6 +64,16 @@ public class DataInitializer implements ApplicationRunner {
             statusData.setSlug(slug);
             var status = taskStatusMapper.map(statusData);
             taskStatusRepository.save(status);
+        });
+    }
+
+    private void addDefaultLabels() {
+        List<String> defaultLabels = List.of("feature", "bug");
+        defaultLabels.forEach(name -> {
+            var labelData = new LabelCreateDTO();
+            labelData.setName(name);
+            var label = labelMapper.map(labelData);
+            labelRepository.save(label);
         });
     }
 }
