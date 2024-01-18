d594816f9564737b833ae20c9b425a49cf1922ff
Alex <alextetekin@gmail.com>
2024-01-18 10:00:18 +0300

    build: added connection with label.

--- src/main/java/hexlet/code/app/model/Task.java
+++ src/main/java/hexlet/code/app/model/Task.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -15,6 +16,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -45,5 +48,8 @@ public class Task implements BaseEntity {
 
     @ManyToOne(fetch = FetchType.EAGER)
     private User assignee;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Label> labels = new HashSet<>();
 
 }
