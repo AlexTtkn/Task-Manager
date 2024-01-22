40c0dca72f2095ae488fb83b154d09db9efa8433
Alex <alextetekin@gmail.com>
2024-01-22 11:33:27 +0300

    fix: changed models

--- src/main/java/hexlet/code/app/model/Label.java
+++ src/main/java/hexlet/code/app/model/Label.java
@@ -13,6 +13,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -36,6 +37,6 @@ public class Label implements BaseEntity {
     @CreatedDate
     private LocalDate createdAt;
 
-    private List<Task> tasks;
+    private List<Task> tasks = new ArrayList<>();
 
 }
